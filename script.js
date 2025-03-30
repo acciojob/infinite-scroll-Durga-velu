8c32911ae8b3c60fab9e621e0a5612f01df080f5
Durga-velu <durgadevi134babu@gmail.com>
2025-03-30 11:17:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
 
+let container = document.querySelector('#infi-list');
+let item = 1;
+
+window.addEventListener('scroll', function(){
+  let scrollY = window.scrollY;
+  let innerHeight = window.innerHeight;
+  let offsetHeight = document.body.offsetHeight;
+  
+  if (scrollY + innerHeight >= offsetHeight) {
+    infiniteScroll(2);
+  }
+});
+
+infiniteScroll(10);
+
+function infiniteScroll(num) {
+  for (let i = 0; i < num; i++) {
+    let li = document.createElement('li');
+    li.innerText = 'List Item ' + item++;
+    container.appendChild(li);
+  }
+}
